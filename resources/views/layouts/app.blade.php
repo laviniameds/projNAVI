232ab86792f96ec808ef38a4c660d049eb59b95d
Lavinia Medeiros <laviniameds@icloud.com>
2017-06-12 12:14:03 -0300

    autenticacao e cadastros prontos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('register') }}">Cadastro</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -63,7 +63,7 @@
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
-                                    <li><a href="{{ url('/home') }}">Editar Informações</a></li>
+                                    <li><a href="{{ url('/perfil') }}">Editar Informações</a></li>
                                 </ul>
                             </li>
                         @endif
