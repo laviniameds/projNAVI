017c836270d2cf6f172ad8952a01931d4e99aa62
Lavinia Medeiros <laviniameds@icloud.com>
2017-06-12 11:01:36 -0300

    complemento do formulario de cadastro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,15 +10,15 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                        <div class="form-group{{ $errors->has('nome') ? ' has-error' : '' }}">
+                            <label for="nome" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="nome" type="text" class="form-control" name="nome" value="{{ old('nome') }}" required autofocus>
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('nome'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('nome') }}</strong>
                                     </span>
                                 @endif
                             </div>
@@ -38,8 +38,188 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('cpf') ? ' has-error' : '' }}">
+                            <label for="cpf" class="col-md-4 control-label">CPF</label>
+
+                            <div class="col-md-6">
+                                <input id="cpf" type="text" class="form-control" name="cpf" value="{{ old('cpf') }}"
+                                data-mask="000.000.000-00" data-mask-reverse="true" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" required>
+
+                                @if ($errors->has('cpf'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('cpf') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('rg') ? ' has-error' : '' }}">
+                            <label for="rg" class="col-md-4 control-label">RG</label>
+
+                            <div class="col-md-6">
+                                <input id="rg" type="text" class="form-control" name="rg" value="{{ old('rg') }}"
+                                data-mask="000.000.000" data-mask-reverse="true" pattern="\d{3}\.\d{3}\.\d{3}" required>
+
+                                @if ($errors->has('rg'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('rg') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('nasc') ? ' has-error' : '' }}">
+                            <label for="nasc" class="col-md-4 control-label">Data de Nascimento</label>
+
+                            <div class="col-md-6">
+                                <input id="nasc" type="date" class="form-control" name="nasc" value="{{ old('nasc') }}" required>
+
+                                @if ($errors->has('nasc'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('nasc') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('genero') ? ' has-error' : '' }}">
+                            <label for="genero" class="col-md-4 control-label">Gênero</label>
+
+                            <div class="col-md-6">
+                                <label>Masculino
+                                <input id="genero" type="radio" class="form-control" name="genero" value="{{ old('genero') }}" required>
+                                </label>
+                                <label>Feminino
+                                <input id="genero" type="radio" class="form-control" name="genero" value="{{ old('genero') }}" required>
+                                </label>
+                                <label>Outro
+                                <input id="genero" type="radio" class="form-control" name="genero" value="{{ old('genero') }}" required>
+                                </label>
+                                @if ($errors->has('genero'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('genero') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('cep') ? ' has-error' : '' }}">
+                            <label for="cep" class="col-md-4 control-label">CEP</label>
+
+                            <div class="col-md-6">
+                                <input id="cep" type="text" class="form-control" name="cep" value="{{ old('cep') }}"
+                                data-mask="00000-000" pattern="\d{5}-\d{3}" required>
+
+                                @if ($errors->has('cep'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('cep') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('rua') ? ' has-error' : '' }}">
+                            <label for="rua" class="col-md-4 control-label">Rua</label>
+
+                            <div class="col-md-6">
+                                <input id="rua" type="text" class="form-control" name="rua" value="{{ old('rua') }}" required>
+
+                                @if ($errors->has('rua'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('rua') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('tipo') ? ' has-error' : '' }}">
+                            <label for="tipo" class="col-md-4 control-label">Tipo de logradouro</label>
+
+                            <div class="col-md-6">
+                                <input id="tipo" type="text" class="form-control" name="tipo" value="{{ old('tipo') }}" required>
+
+                                @if ($errors->has('tipo'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('tipo') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('numero') ? ' has-error' : '' }}">
+                            <label for="numero" class="col-md-4 control-label">Número</label>
+
+                            <div class="col-md-6">
+                                <input id="numero" type="text" class="form-control" name="numero" value="{{ old('numero') }}" required>
+
+                                @if ($errors->has('numero'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('numero') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('complemento') ? ' has-error' : '' }}">
+                            <label for="complemento" class="col-md-4 control-label">Complemento</label>
+
+                            <div class="col-md-6">
+                                <input id="complemento" type="text" class="form-control" name="complemento" value="{{ old('complemento') }}" required>
+
+                                @if ($errors->has('complemento'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('complemento') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('bairro') ? ' has-error' : '' }}">
+                            <label for="bairro" class="col-md-4 control-label">Bairro</label>
+
+                            <div class="col-md-6">
+                                <input id="bairro" type="text" class="form-control" name="bairro" value="{{ old('bairro') }}" required>
+
+                                @if ($errors->has('bairro'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bairro') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('cidade') ? ' has-error' : '' }}">
+                            <label for="cidade" class="col-md-4 control-label">Cidade</label>
+
+                            <div class="col-md-6">
+                                <input id="cidade" type="text" class="form-control" name="cidade" value="{{ old('cidade') }}" required>
+
+                                @if ($errors->has('cidade'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('cidade') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('estado') ? ' has-error' : '' }}">
+                            <label for="estado" class="col-md-4 control-label">Estado</label>
+
+                            <div class="col-md-6">
+                                <input id="estado" type="text" class="form-control" name="estado" value="{{ old('estado') }}"
+                                maxlength="2" required>
+
+                                @if ($errors->has('estado'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('estado') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">Senha</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -53,7 +233,7 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <label for="password-confirm" class="col-md-4 control-label">Confirmação da senha</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
